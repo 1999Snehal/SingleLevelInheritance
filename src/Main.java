eb18f7a130b7ffe876454c8c914d5a81acbbb426
1999Snehal <snehallagad84@gmail.com>
2024-11-05 16:08:45 +0530

    commited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+class Shape1
+{
+    public  void  area()
+    {
+
+        System.out.println("Displays area");
+    }
+}
+  class Triangle extends Shape1
+{
+    public  void area(int l, int h)
+    {
+        l = 4;
+        h = 3;
+        System.out.println(1/2*l*h);
+    }
+}
+public  class Main
+{
+    public static void main(String args[])
+    {
+
     }
 }
